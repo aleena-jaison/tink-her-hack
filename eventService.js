e9043740adf08a5f66df27a155fa352465e75729
Aleena Jaison <149742345+aleena-jaison@users.noreply.github.com>
2025-01-25 19:09:20 +0530

    deleted

--- eventService.js
+++ eventService.js
@@ -1,10 +1,1 @@
-import axios from 'axios';
 
-export const getEventsNearby = async (lat, long) => {
-    try {
-        const response = await axios.get(`/api/events?lat=${lat}&long=${long}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching events:', error);
-    }
-};
